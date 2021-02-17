25add3a584ad93758a12929a2f5af1249b986082
Edward Boland <mr.eboland@gmail.com>
2021-02-17 17:22:55 -0500

    created urls and views for our api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# Importing Django RESTFramework's generics class of views
+from rest_framework import generics
+# Import the models from our books app
+from books.models import Book
+# Importing serializers from our api app
+from .serializers import BookSerializer
 
-# Create your views here.
+# Creatign a class that uses ListAPIView to create a read-only endpoint for all book instances.
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
