2d9da2cf2dcfcf57c8ae1359553236a199095a92
Edward Boland <mr.eboland@gmail.com>
2021-02-17 17:09:36 -0500

    added html for homepage to list books

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from .views import BookListView
 
 urlpatterns = [
     # Empty string indicates our view is what is shown on home. Gave it a named URL of 'home' to reference.
-    # The full 
+    # The full pathing when a user goes to the homepage of our site is they will first hit the config/urls.py file, then be redirected to books/urls.py which specifies using the BookListView. In this file, the Book model is used along with ListView to list out all books.
     path("", BookListView.as_view(), name="home"),
 ]
